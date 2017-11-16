b40d74a31e054096ed68c3a8cd4093233405a5e2
Gina Antonini <ginamantonini@gmail.com>
2017-11-15 21:50:14 -0600

    delete movie is working

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -1,10 +1,29 @@
 "use strict";
 
-app.controller("WishlistCtrl", function($rootScope, $scope, WishlistService){
-   WishlistService.getWishlistMovies($rootScope.uid).then((results) => {
+app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
+
+const getMovies = () => {
+		MovieService.getWishlistMovies($rootScope.uid).then((results) => {
+   		$scope.movies = results;
+   		}).catch((err) => {
+   		console.log("error in getRatedMovies", err);
+   		});
+	};
+
+	getMovies();
+
+   MovieService.getWishlistMovies($rootScope.uid).then((results) => {
    	$scope.movies = results;
    }).catch((err) => {
    		console.log("error in getRatedMovies", err);
    });
+
+   $scope.deleteMovie = (movieId) => {
+   	MovieService.deleteMovie(movieId).then((result) => {
+   		getMovies();
+   	}).catch((err) => {
+   		console.log("error in deleteMovie", err);
+   	});
+   };
 });
 
--- javascripts/services/WishlistService.js
+++ javascripts/services/WishlistService.js
@@ -1,22 +1,6 @@
-"use strict";
+// "use strict";
 
-app.service("WishlistService", function($http, $q, FIREBASE_CONFIG){
-const getWishlistMovies = (userUid) => {
-		let movies = [];
-		return $q((resolve, reject) => {
-			$http.get(`${FIREBASE_CONFIG.databaseURL}/movies.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
-				let fbMovies = results.data;
-				Object.keys(fbMovies).forEach((key) =>{
-					fbMovies[key].id = key;  // fbMovies["movies0"].id = "movies0"
-					if(!fbMovies[key].isWatched){
-						movies.push(fbMovies[key]);
-					}
-					resolve(movies);
-				});
-			}).catch((err) => {
-				reject(err);
-			});
-		});
-	};
-	return {getWishlistMovies};
-});
+// app.service("WishlistService", function($http, $q, FIREBASE_CONFIG){
+
+// 	return {getWishlistMovies};
+// });
