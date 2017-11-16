360a6d7817686044441199e735c690c193b237f9
Gina Antonini <ginamantonini@gmail.com>
2017-11-15 20:56:53 -0600

    saving rated movies to firebase

--- javascripts/services/MovieService.js
+++ javascripts/services/MovieService.js
@@ -19,6 +19,11 @@ app.service("MovieService", function($http, $q, FIREBASE_CONFIG){
 		});
 		
 	};
-	return {getRatedMovies};
+
+	const postNewMovie = (newMovie) => {
+		return $http.post(`${FIREBASE_CONFIG.databaseURL}/movies.json`, JSON.stringify(newMovie));
+	};
+
+	return {getRatedMovies, postNewMovie};
 });
 
